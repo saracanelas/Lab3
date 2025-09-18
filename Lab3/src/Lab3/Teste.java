4588dacd261df21221177aeda083e95717ef99d5
saracanelas <sara.i.canelas@gmail.com>
2025-09-18 17:43:39 +0100

    Segundo commit - Atualização do gitIgnore e foi criada a classe Teste

---
+++ Lab3/src/Lab3/Teste.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package Lab3;
+
+/**
+ * 
+ */
+public class Teste {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
