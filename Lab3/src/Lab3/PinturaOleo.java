69982bc2ed9ac1cd552aa5f3849fcc3b9c8cd3a1
Beatriz Antunes <biasoaresantunes15@gmail.com>
2025-09-19 00:57:06 +0100

    Atributo e construtor da classe PinturaOleo.

--- Lab3/src/Lab3/PinturaOleo.java
+++ Lab3/src/Lab3/PinturaOleo.java
@@ -5,5 +5,15 @@ package Lab3;
  */
 
 public class PinturaOleo {
+	private String tipoTela;
 
+	/**
+	 * @param tipoTela
+	 */
+	
+	public PinturaOleo(String tipoTela) {
+		this.tipoTela = tipoTela;
+	}
+	
+	
 }
