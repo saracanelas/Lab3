2940c4ca494d74110bc499b62615261bb3f0e437
Beatriz Pimentel <Beatriz Pimentel@Lua.home>
2025-09-18 21:16:36 +0100

    Atualização dos métodos - retirar os métodos de comentário

--- Lab3/src/Lab3/GestorObra.java
+++ Lab3/src/Lab3/GestorObra.java
@@ -9,16 +9,23 @@ import java.util.ArrayList;
  * 
  */
 public class GestorObra {
-	/* private ArrayList <Obra>obras; */
+	 private ArrayList <Obra>obras; 
 	
-	/*
-	 * public void adicionarObra(Obra o) { obras.add(o); }
-	 */
 	
-	/*
-	 * public Obra pesquisarTitulo (String titulo) { for (Obra o : obras) if
-	 * (o.getTitulo().equalsIgnoreCase(titulo)) { return o; }
-	 * System.out.println("Obra não encontrada"); return null; }
-	 */
-
+	  public void adicionarObra(Obra o) { 
+		  obras.add(o); 
+		  }
+	 
+	
+	
+	  public Obra pesquisarTitulo (String titulo) { 
+		  for (Obra o : obras) 
+			  if (o.getTitulo().equalsIgnoreCase(titulo)) {
+				  return o; 
+				  }
+	  System.out.println("Obra não encontrada"); 
+	  return null; 
+	  }
+	 
+	  
 }
