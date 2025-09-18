c043f082d76fa8ea966afda00d951ff411aec99d
Beatriz Antunes <biasoaresantunes15@gmail.com>
2025-09-19 00:42:09 +0100

    Atributos e construtor da classe pintura.

--- Lab3/src/Lab3/Pintura.java
+++ Lab3/src/Lab3/Pintura.java
@@ -7,6 +7,21 @@ package Lab3;
  * 
  */
 
-public class Pintura {
+import java.util.ArrayList;
 
+public class Pintura {
+	private String tecnica;
+	private ArrayList<PinturaOleo> oleos;
+	
+	/**
+	 * @param tecnica
+	 * @param oleos
+	 */
+	
+	public Pintura(String tecnica, ArrayList<PinturaOleo> oleos) {
+		this.tecnica = tecnica;
+		this.oleos = oleos;
+	}
+	
+	
 }
