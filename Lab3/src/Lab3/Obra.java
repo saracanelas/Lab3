1f9d9734ec82c98a6788d5075a2c7b9cc0e7049a
chiicaa15 <joaomarques0243@gmail.com>
2025-09-18 20:41:07 +0100

    Criação da classe Obra e criação do construtor

---
+++ Lab3/src/Lab3/Obra.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package Lab3;
+import java.util.ArrayList;
+/**
+ * 
+ */
+public class Obra {
+	private String titulo;
+	private String autor;
+	private int data;
+	private ArrayList <Pintura> pinturas;
+	private ArrayList <Escultura> esculturas;
+	/**
+	 * @param titulo
+	 * @param autor
+	 * @param data
+	 */
+	public Obra(String titulo, String autor, int data) {
+		this.titulo = titulo;
+		this.autor = autor;
+		this.data = data;
+		pinturas = new ArrayList <Pintura>;
+		esculturas = new ArrayList <Escultura>;
+	
+	}
+	
+	
+}
