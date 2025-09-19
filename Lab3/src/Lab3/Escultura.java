6f591d6fb6757919e5b050ed007db23ef4c25b06
Beatriz Antunes <biasoaresantunes15@gmail.com>
2025-09-19 01:05:45 +0100

    Atributo e constutor da classe escultura.

--- Lab3/src/Lab3/Escultura.java
+++ Lab3/src/Lab3/Escultura.java
@@ -8,5 +8,13 @@ package Lab3;
  */
 
 public class Escultura {
+	private String material;
 
+	/**
+	 * @param material
+	 */
+	public Escultura(String material) {
+		this.material = material;
+	}
+	
 }
