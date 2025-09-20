c6263fb0535c7a8f08d65045ef9409baa300a2d9
saracanelas <sara.i.canelas@gmail.com>
2025-09-20 20:59:49 +0100

    Classe Teste - Primeira atualização

--- Lab3/src/Lab3/Teste.java
+++ Lab3/src/Lab3/Teste.java
@@ -3,6 +3,8 @@
  */
 package Lab3;
 
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -12,7 +14,27 @@ public class Teste {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+GestorObra g = new GestorObra();
+		
+		Scanner input = new Scanner(System.in);
+		int opção;
+		
+		do {
+			System.out.println("Bem vindo ao catálogo do Museu");
+			System.out.println("1. Registar obra");
+			System.out.println("2. Imprimir todos os detalhes de todas as obras");
+			System.out.println("3. Pesquisar por obra");
+			System.out.println("4. Imprimir detalhes de obras por tela");
+			System.out.println("5. Imprimir todos os detalhes de todas as obras de arte agrupadas por tipo, indicando o número total de cada tipo");
+			System.out.println("6. Sair");
+			System.out.println("Opção: ");
+			opção = input.nextInt();
+			
+			
+		
+	
+		}			
+	
 
 	}
 
